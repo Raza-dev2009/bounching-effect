d881d779c6ff9303140fdaba7507b40979659e4c
Raza-dev2009 <arfangol37@gmail.com>
2024-10-09 07:05:55 -0700

    Add files via upload
    https://github.com/Raza-dev2009/bounching-effect.git

---
+++ script.js
@@ -0,0 +1,24 @@
+// $("#change").on('click', function(){
+//     const color1 = $('#color1').val();
+
+//     const color2 = $('#color2').val();  
+//     $('letter').css('transform: translateY',`transition-delay: 0.2s;`)
+
+
+//     // $("#change").on('click', function(){
+//     //     const color2 = $('#color2').val();
+
+//     //     $("#change").on('click', function(){
+//     //         const color3 = $('#color3').val();
+
+//     //         $("#change").on('click', function(){
+//     //             const color4 = $('#color4').val();
+
+//     //             $("#change").on('click', function(){
+//     //                 const color5 = $('#color5').val();
+
+//     //             })
+//     //         })
+//     //     })
+//     // })
+// })
